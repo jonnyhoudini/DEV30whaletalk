34a3bc2acdeee2b8ba926e2b25f03d1385c3f169
jonnyhoudini <jmcaveety@gmail.com>
2022-03-15 06:41:09 +0000

    adding html and js

---
+++ main.js
@@ -0,0 +1,21 @@
+let input = 'There is nothing noble in being superior to your fellow man. True nobility is being superior to your former self.'
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+    let character = input.charAt(i);
+    character.toLowerCase();
+    if (character === 'e' || character === 'u') {
+        resultArray.push(character)
+    }
+    for (let j = 0; j < vowels.length; j++) {
+        const vowel = vowels[j];
+        if (character === vowel) {
+            resultArray.push(vowel);
+        }
+    }
+}
+
+// I have just finished step 11 of the Whale Talk project
